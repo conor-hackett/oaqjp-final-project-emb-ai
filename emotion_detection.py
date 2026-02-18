8fea72fde9423c683fd36dc8cbd16e6c0cc1d6de
conor-hackett <hackettcp12@gmail.com>
2026-02-18 04:40:09 -0500

    Formatted emotion detector response

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,5 +6,22 @@ def emotion_detector(text_to_analyse):
     my_json = { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, json=my_json, headers=headers)
+    formatted_response = json.loads(response.text)
 
-    return response.text
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion' :dominant_emotion 
+    }
